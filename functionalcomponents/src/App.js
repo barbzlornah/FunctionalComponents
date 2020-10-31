a3ed492429649ddfe2ec495c2a5c14f7f7026c7d
Barbara Masinde <barbzlornah@gmail.com>
2020-10-31 18:59:46 +0300

    new commit

---
+++ functionalcomponents/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React ,{useState} from "react"; 
+  
+function FunctionalComponent(){ 
+    const[count , setCount]=useState(0); 
+  
+    function increase(){ 
+        setCount(count+1); 
+    } 
+  
+    return ( 
+        <div style={{margin:'50px'}}> 
+            <h1>Welcome to Geeks for Geeks </h1> 
+            <h3>Counter App using Functional Component : </h3> 
+            <h2>{count}</h2> 
+            <button onClick={increase}>Add</button> 
+        </div> 
+    ) 
+} 
+  
+export default FunctionalComponent;
